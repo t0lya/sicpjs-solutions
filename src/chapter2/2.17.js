7816c485046151cd8099b23e53ef0ef78bdf70e0
let00 <tony.le@usa.com>
2020-09-07 15:42:23 -0400

    2.17

---
+++ src/chapter2/2.17.js
@@ -0,0 +1,17 @@
+function pair(a, b) {
+  return [a, b]
+}
+
+function head(pair) {
+  return pair[0]
+}
+
+function tail(pair) {
+  return pair[1]
+}
+
+function last(list) {
+  return tail(list) === null ? head(list) : last(tail(list))
+}
+
+console.log(last(pair(3434, pair(652, pair(8345, pair(3267, pair(6, null)))))))
